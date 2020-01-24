634340f86ecc5f141d184828e69af492d182ffb3
rocketpy <dimitr.ushakov@gmail.com>
2020-01-24 23:30:43 +0200

    modified

--- example_2.py
+++ example_2.py
@@ -1,1 +1,20 @@
+import re
+import collections
 
+
+with open('file_name.txt') as f:
+text = f.read()
+
+words = re.compile(r"a-zA-Z'").findall(text)
+counts = collections.Counter(words)
+
+print(counts.most_common(10)) 
+
+
+#  one more example
+from collections import Counter
+
+with open('file_name.txt') as fin:
+    counter = Counter(fin.read().strip().split())
+
+print(counter.most_common())

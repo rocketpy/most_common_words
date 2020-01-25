08d142268e8d3ae8089197c1d2ad79a2a4475eaf
rocketpy <dimitr.ushakov@gmail.com>
2020-01-25 22:38:11 +0200

    modified

--- example_3.py
+++ example_3.py
@@ -1,3 +1,8 @@
 import re
 import collections
 
+
+words = re.findall(r'\w+', open(file_name.txt).read().lower() )
+most_common_words = collections.Counter(words).most_common(20)  #  20 - words
+
+print(most_common_words)
